879415e74d9fc3f422e421aadf4a760c2cc73b3d
Gourav <gouravsuram91@gmail.com>
2022-07-23 18:42:15 +0530

    Login Redirection

---
+++ logincheck.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+include 'imports/config.php';
+
+if(isset($_POST['login'])){
+
+    $uname = $_POST['uname'];
+    $passwd = $_POST['passwd'];
+
+    $sql = "SELECT * FROM login WHERE `uname` = '$uname' AND `passwd` = '$passwd'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $count = mysqli_num_rows($result);
+
+    if($count == 1){
+
+        if($row['role'] == 'Admin'){
+            $_SESSION['uname'] = $uname;
+            $_SESSION['role'] = 'Admin';
+            header("Location: roles/admin/index.php");
+        }
+        else if($row['role'] == 'Faculty'){
+            $_SESSION['uname'] = $uname;
+            $_SESSION['role'] = 'Faculty';
+            header("Location: roles/faculty/index.php");
+        }
+        else{
+            $_SESSION['uname'] = $uname;
+            $_SESSION['role'] = 'Student';
+            header("Location: roles/student/index.php");
+        }
+    }else{
+        echo "<script>alert('Invalid username or password');</script>";
+        echo "<script>window.location.href='index.php';</script>";
+    }
+}
+
+
+?>
--- roles/student/index.php
+++ roles/student/index.php
@@ -2,12 +2,12 @@
 
 session_start();
 
-if($_SESSION['role'] != 'Admin'){
+if($_SESSION['role'] != 'Student'){
    echo "<script>alert('Invalid Session, please login again');</script>";
    echo "<script>window.location.href='index.php';</script>";
 }else{
 
-   echo "Admin index";
+   echo "Student index";
 }
 
 ?>
