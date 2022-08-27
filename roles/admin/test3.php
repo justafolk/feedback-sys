1be8e875fb89aaa18db561148ef943ee1f76bfdb
Mayur Khadde <102829101+MK-C0dE5@users.noreply.github.com>
2022-08-27 23:37:40 +0530

    27-08

--- roles/admin/test3.php
+++ roles/admin/test3.php
@@ -1,71 +1,97 @@
-<div id="profile1">
-							<div class="row mt-3">
-								<div class="col-md-12 mb-3">
-									<div class="input-group">
-										<input type="text" class="form-control" placeholder="Search for ....">
-										<span class="input-group-btn">
-											<button class="btn btn-secondary" type="button">Search</button>
-										</span>
-									</div>
-                                    <div class="mt-3">
-                                        <button class="btn btn-primary me-3" onclick="show1()" >Bar Graph</button>
-                                        <button class="btn btn-primary me-3" onclick="show2()" >Pie Graph</button>
-                                        <button class="btn btn-primary me-3" onclick="show3()" >List Form</button>
-                                    </div>
-								</div>
-								<div class="col-12 col-md-12 col-xxl-6 order-1 order-xxl-1">
-									<div class="card flex-fill w-200">
-										<div class="card-header d-flex justify-content-between">
-											<div>
-												<h5 class="card-title mb-0">PHP - Feedback</h5>
-												<h6 class="card-title mb-0">26/03/2022</h6>
-											</div>
-											<button class="btn btn-primary" id="id4">View</button>
-										</div>
-										<div class="card-body d-flex">
-											<canvas id="barChart" style="max-height: 800px;"></canvas>
-											<script>
-												document.addEventListener("DOMContentLoaded", () => {
-												new Chart(document.querySelector('#barChart'), {
-													type: 'bar',
-													data: {
-													labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-													datasets: [{
-														label: 'Bar Chart',
-														data: [65, 59, 80, 81, 56, 55, 40],
-														backgroundColor: [
-														'rgba(255, 99, 132, 0.2)',
-														'rgba(255, 159, 64, 0.2)',
-														'rgba(255, 205, 86, 0.2)',
-														'rgba(75, 192, 192, 0.2)',
-														'rgba(54, 162, 235, 0.2)',
-														'rgba(153, 102, 255, 0.2)',
-														'rgba(201, 203, 207, 0.2)'
-														],
-														borderColor: [
-														'rgb(255, 99, 132)',
-														'rgb(255, 159, 64)',
-														'rgb(255, 205, 86)',
-														'rgb(75, 192, 192)',
-														'rgb(54, 162, 235)',
-														'rgb(153, 102, 255)',
-														'rgb(201, 203, 207)'
-														],
-														borderWidth: 1
-													}]
-													},
-													options: {
-													scales: {
-														y: {
-														beginAtZero: true
-														}
-													}
-													}
-												});
-												});
-											</script>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<title>Test</title>
+</head>
+<body>
+<form method="post" id="user_form">
+	<table class="table text-center">
+		<thead class="thead-dark">
+			<tr>
+				<th scope="col">Sr. No</th>
+				<th scope="col">Subject</th>
+				<th scope="col">Course Code</th>
+				<th scope="col">Teacher Name</th>
+				<th scope="col">Total Questions</th>
+				<th scope="col">Status</th>
+				<th scope="col">Action</th>
+				<th scope="col">View</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">1</th>
+				<td>Basic Mathematics</td>
+				<td>R18SC1701</td>
+				<td>@mdo</td>
+				<td>7</td>
+				<th>Not Done</th>
+				<input type="hidden" name="name" value="Basic Mathematics">
+				<b id="msg"></b>
+				<td><input class="btn btn-danger" type="button" name="id1" id="id1" value="Inactive"></td>
+				<td><button type="button" class="btn btn-primary" id="id2">View1</button></td>
+			</tr>
+			<tr>
+				<th scope="row">2</th>
+				<td>Basic Physics</td>
+				<td>R18SC1703</td>
+				<td>@mdo</td>
+				<td>7</td>
+				<th>Done</th>
+				<td><button class="btn btn-primary">Allow Access</button></td>
+				<td><button class="btn btn-primary" id="id3">View</button></td>
+			</tr>
+			<tr>
+				<th scope="row">3</th>
+				<td>English</td>
+				<td>R18SC1707</td>
+				<td>@mdo</td>
+				<td>7</td>
+				<td>Not Done</td>
+				<td><button class="btn btn-primary">Allow Access</button></td>
+				<td><button class="btn btn-primary" id="id4">View</button></td>
+			</tr>
+			<tr>
+				<th scope="row">4</th>
+				<td>Engineering Graphic Skills</td>
+				<td>R18ME1210</td>
+				<td>@mdo</td>
+				<td>7</td>
+				<td>Not Done</td>
+				<td><button class="btn btn-primary">Allow Access</button></td>
+				<td><button class="btn btn-primary" id="id5">View</button></td>
+			</tr> 
+			<tr>
+				<th scope="row">5</th>
+				<td>Programming in C</td>
+				<td>R18CP3401</td>
+				<td>@mdo</td>
+				<td>7</td>
+				<td>Not Done</td>
+				<td><button class="btn btn-primary">Allow Access</button></td>
+				<td><button class="btn btn-primary" id="id6">View</button></td>
+			</tr>
+		</tbody>
+	</table>
+</form>
+<script>
+	$(document).ready(function(){
+		$("#id1").click(function(){
+			var data = $("#user_form").serialize()+"&id1=id1";
+			$.ajax({
+				url:"insert_data.php",
+				type:"post",
+				data:data,
+				success:function(response){
+					$("#msg").text(response);
+				}
+			});
+		});
+	})
+</script>
+</body>
+</html>
